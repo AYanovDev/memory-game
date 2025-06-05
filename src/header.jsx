638608e9e8d28da25cca76577886cda66507ef57
AYanovDev <alex.ianovskii@mail.ru>
2025-06-05 14:21:09 +0500

    Styling done

--- src/header.jsx
+++ src/header.jsx
@@ -1,3 +1,13 @@
+import "./header.css";
+
 export default function Header() {
-  return <header>Memory game</header>;
+  return (
+    <>
+      <header>The Memory Game</header>
+      <p>
+        Test your memory by clicking the words only once. Try not to click the
+        same word twice!
+      </p>
+    </>
+  );
 }
